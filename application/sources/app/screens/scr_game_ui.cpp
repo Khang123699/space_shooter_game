79651642fab6ecc3a51404d70bb7a44eb7aeb614
Khang Nguyen <khangnbk2004@gmail.com>
2026-07-08 11:04:13 +0700

    [UPDATE] Update view_scr_game_ui function

--- application/sources/app/screens/scr_game_ui.cpp
+++ application/sources/app/screens/scr_game_ui.cpp
@@ -26,6 +26,8 @@ view_screen_t scr_game_ui = {
 
 void view_scr_game_ui() {
 	view_render.clear();
+	view_render.setTextSize(1);
+	view_render.setTextColor(WHITE);
 	
 	if (g_game_state == GAME_STATE_MENU) {
 		view_render.drawRect(0, 0, 128, 64, WHITE);
