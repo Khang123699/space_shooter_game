2f60f601a98c456847e42239555b853bae0c685f
Khang Nguyen <khangnbk2004@gmail.com>
2026-07-08 10:11:12 +0700

    [ADD] Implement core physics initialization and variable definitions

---
+++ application/sources/app/space_shooter/game_shooter_logic.cpp
@@ -0,0 +1,50 @@
+#include "game_shooter.h"
+#include "task_list.h"
+#include "timer.h"
+#include "buzzer.h"
+#include <string.h>
+#include <stdlib.h>
+#include "app_dbg.h"
+
+// Global game state variables
+game_state_t g_game_state = GAME_STATE_MENU;
+int16_t g_player_x = 60;
+uint8_t g_player_blink = 0;
+uint32_t g_score = 0;
+int8_t g_lives = 3;
+enemy_t g_enemies[MAX_ENEMIES];
+bullet_t g_bullets[MAX_BULLETS];
+explosion_t g_explosions[MAX_EXPLOSIONS];
+uint8_t g_stage = 1;
+int8_t g_transition_timer = 0;
+
+// Internal function to spawn a grid of enemies
+static void spawn_enemies() {
+	int idx = 0;
+	// Create a 3x6 grid of potential enemies
+	for (int r = 0; r < 3; r++) {
+		for (int c = 0; c < 6; c++) {
+			// 70% chance to spawn an enemy at a grid position
+			if (rand() % 100 > 30) { 
+				g_enemies[idx].active = true;
+				g_enemies[idx].x = 10 + c * 14;
+				g_enemies[idx].y = r * 10 + 12;
+			} else {
+				g_enemies[idx].active = false;
+			}
+			idx++;
+		}
+	}
+}
+
+// Reset all game variables to start a new game
+void game_logic_init() {
+	g_player_x = 60;
+	g_score = 0;
+	g_lives = 3;
+	g_stage = 1;
+	g_transition_timer = 0;
+	memset(g_bullets, 0, sizeof(g_bullets));
+	memset(g_explosions, 0, sizeof(g_explosions));
+	spawn_enemies();
+}
