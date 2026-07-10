1e4f6fbc336ac9e2471b06c4ef0e920b61ef48f9
Khang Nguyen <khangnbk2004@gmail.com>
2026-07-10 09:56:40 +0700

    [UPDATE] Format menu logic

--- application/sources/app/screens/scr_game_ui_handle.cpp
+++ application/sources/app/screens/scr_game_ui_handle.cpp
@@ -6,32 +6,64 @@
 #include "task_list.h"
 
 static void handle_ui_menu_input(ak_msg_t *msg) {
-	if (msg->sig == AC_DISPLAY_BUTON_UP_PRESSED) { g_menu_selected = (g_menu_selected > 0) ? g_menu_selected - 1 : 2; }
-	else if (msg->sig == AC_DISPLAY_BUTON_DOWN_PRESSED) { g_menu_selected = (g_menu_selected < 2) ? g_menu_selected + 1 : 0; }
-	else if (msg->sig == AC_DISPLAY_BUTON_MODE_PRESSED) {
-		if (g_menu_selected == 0) { g_game_state = GAME_STATE_PLAYING; task_post_pure_msg(AC_TASK_GAME_SHOOTER_ID, AC_GAME_START_REQ); }
-		else if (g_menu_selected == 1) { g_game_state = GAME_STATE_SETTING; g_setting_selected = 0; }
-		else if (g_menu_selected == 2) { g_game_state = GAME_STATE_HIGH_SCORE; g_score_selected = 0; }
+	switch (msg->sig) {
+		case AC_DISPLAY_BUTON_UP_PRESSED:
+			g_menu_selected = (g_menu_selected > 0) ? g_menu_selected - 1 : 2;
+			break;
+		case AC_DISPLAY_BUTON_DOWN_PRESSED:
+			g_menu_selected = (g_menu_selected < 2) ? g_menu_selected + 1 : 0;
+			break;
+		case AC_DISPLAY_BUTON_MODE_PRESSED:
+			if (g_menu_selected == 0) { 
+				g_game_state = GAME_STATE_PLAYING; 
+				task_post_pure_msg(AC_TASK_GAME_SHOOTER_ID, AC_GAME_START_REQ); 
+			} else if (g_menu_selected == 1) { 
+				g_game_state = GAME_STATE_SETTING; 
+				g_setting_selected = 0; 
+			} else if (g_menu_selected == 2) { 
+				g_game_state = GAME_STATE_HIGH_SCORE; 
+				g_score_selected = 0; 
+			}
+			break;
 	}
 }
 
 static void handle_ui_setting_input(ak_msg_t *msg) {
-	if (msg->sig == AC_DISPLAY_BUTON_UP_PRESSED) { g_setting_selected = (g_setting_selected > 0) ? g_setting_selected - 1 : 2; }
-	else if (msg->sig == AC_DISPLAY_BUTON_DOWN_PRESSED) { g_setting_selected = (g_setting_selected < 2) ? g_setting_selected + 1 : 0; }
-	else if (msg->sig == AC_DISPLAY_BUTON_MODE_PRESSED) {
-		if (g_setting_selected == 0) { g_game_data.sound_en = !g_game_data.sound_en; game_save_data(); }
-		else if (g_setting_selected == 1) { g_game_data.difficulty = (g_game_data.difficulty + 1) % 3; game_save_data(); }
-		else if (g_setting_selected == 2) { g_game_state = GAME_STATE_MENU; }
+	switch (msg->sig) {
+		case AC_DISPLAY_BUTON_UP_PRESSED:
+			g_setting_selected = (g_setting_selected > 0) ? g_setting_selected - 1 : 2;
+			break;
+		case AC_DISPLAY_BUTON_DOWN_PRESSED:
+			g_setting_selected = (g_setting_selected < 2) ? g_setting_selected + 1 : 0;
+			break;
+		case AC_DISPLAY_BUTON_MODE_PRESSED:
+			if (g_setting_selected == 0) { 
+				g_game_data.sound_en = !g_game_data.sound_en; 
+				game_save_data(); 
+			} else if (g_setting_selected == 1) { 
+				g_game_data.difficulty = (g_game_data.difficulty + 1) % 3; 
+				game_save_data(); 
+			} else if (g_setting_selected == 2) { 
+				g_game_state = GAME_STATE_MENU; 
+			}
+			break;
 	}
 }
 
 static void handle_ui_highscore_input(ak_msg_t *msg) {
-	if (msg->sig == AC_DISPLAY_BUTON_UP_PRESSED || msg->sig == AC_DISPLAY_BUTON_DOWN_PRESSED) {
-		g_score_selected = (g_score_selected == 0) ? 1 : 0;
-	}
-	else if (msg->sig == AC_DISPLAY_BUTON_MODE_PRESSED) {
-		if (g_score_selected == 0) { init_default_save_data(); game_save_data(); }
-		else if (g_score_selected == 1) { g_game_state = GAME_STATE_MENU; }
+	switch (msg->sig) {
+		case AC_DISPLAY_BUTON_UP_PRESSED:
+		case AC_DISPLAY_BUTON_DOWN_PRESSED:
+			g_score_selected = (g_score_selected == 0) ? 1 : 0;
+			break;
+		case AC_DISPLAY_BUTON_MODE_PRESSED:
+			if (g_score_selected == 0) { 
+				init_default_save_data(); 
+				game_save_data(); 
+			} else if (g_score_selected == 1) { 
+				g_game_state = GAME_STATE_MENU; 
+			}
+			break;
 	}
 }
 
@@ -56,12 +88,24 @@ static void handle_ui_gameover_input(ak_msg_t *msg) {
 }
 
 static void handle_ui_showscore_input(ak_msg_t *msg) {
-	if (msg->sig == AC_DISPLAY_BUTON_UP_PRESSED) { g_show_score_selected = (g_show_score_selected > 0) ? g_show_score_selected - 1 : 2; }
-	else if (msg->sig == AC_DISPLAY_BUTON_DOWN_PRESSED) { g_show_score_selected = (g_show_score_selected < 2) ? g_show_score_selected + 1 : 0; }
-	else if (msg->sig == AC_DISPLAY_BUTON_MODE_PRESSED) {
-		if (g_show_score_selected == 0) { g_game_state = GAME_STATE_PLAYING; task_post_pure_msg(AC_TASK_GAME_SHOOTER_ID, AC_GAME_START_REQ); }
-		else if (g_show_score_selected == 1) { g_game_state = GAME_STATE_HIGH_SCORE; g_score_selected = 1; }
-		else if (g_show_score_selected == 2) { g_game_state = GAME_STATE_MENU; }
+	switch (msg->sig) {
+		case AC_DISPLAY_BUTON_UP_PRESSED:
+			g_show_score_selected = (g_show_score_selected > 0) ? g_show_score_selected - 1 : 2;
+			break;
+		case AC_DISPLAY_BUTON_DOWN_PRESSED:
+			g_show_score_selected = (g_show_score_selected < 2) ? g_show_score_selected + 1 : 0;
+			break;
+		case AC_DISPLAY_BUTON_MODE_PRESSED:
+			if (g_show_score_selected == 0) { 
+				g_game_state = GAME_STATE_PLAYING; 
+				task_post_pure_msg(AC_TASK_GAME_SHOOTER_ID, AC_GAME_START_REQ); 
+			} else if (g_show_score_selected == 1) { 
+				g_game_state = GAME_STATE_HIGH_SCORE; 
+				g_score_selected = 1; 
+			} else if (g_show_score_selected == 2) { 
+				g_game_state = GAME_STATE_MENU; 
+			}
+			break;
 	}
 }
 
